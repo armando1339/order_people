064854147d8854dc936d277c31ffdacf2e604e23
Armando Alejandre <armandoalejandre@Miguels-MacBook-Pro.local>
2017-09-12 15:54:45 -0500

    code refactoring

--- order_people.rb
+++ order_people.rb
@@ -1,21 +1,38 @@
+# Class that order a hash of people by name or age.
+#
 class OrderPeople
   def initialize(people = {})
     @people = people
   end
 
-  def sort_by! attribute = "name"
-    attribute = attribute.to_sym
-    @people = @people.sort_by { |k, v| v[attribute] }
-    @people.to_h
+  def sort_by!(attribute = 'name')
+    @people = sort_people_hash(attribute)
+  end
+
+  def sort_by(attribute = 'name')
+    sort_people_hash(attribute)
   end
 
   def sort_by_age!
-    @people = @people.sort_by { |k, v| v[:age] }
-    @people.to_h
+    @people = sort_people_hash('age')
+  end
+
+  def sort_by_age
+    sort_people_hash('age')
   end
 
   def sort_alphabetically_by_name!
-    @people =  @people.sort_by { |k, v| v[:name] }
-    @people.to_h
+    @people = sort_people_hash('name')
+  end
+
+  def sort_alphabetically_by_name
+    sort_people_hash('name')
+  end
+
+  private
+
+  def sort_people_hash(attribute = '')
+    people = @people.sort_by { |_k, v| v[attribute.to_sym] }
+    people.to_h
   end
 end
